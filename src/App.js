9540723d1a6a7f28d6fbfb2497c3953eead8ecab
Nhan Bui <buidev94@gmail.com>
2022-07-23 22:45:56 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -27,26 +27,87 @@ function naturalCompare(s1, s2) {
   });
 }
 
-function App() {
-  const [repos, setRepos] = useState([]);
+function getGroupName(str) {
+  const normalRegex = /(.*) \d*$/;
+  const tankRegex = /(.*) Tank( \d*)?$/;
+
+  const normalMatch = str.match(normalRegex);
+  if (normalMatch != null) {
+    return normalMatch[1];
+  }
+
+  const tankMatch = str.match(tankRegex);
+  if (tankMatch != null) {
+    return tankMatch[1];
+  }
+
+  return str;
+}
+
+function useRepoGroups() {
+  const [repoGroups, setRepoGroups] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const { data: newRepos } = await octokit.rest.repos.listForUser({
+      // Fetch repos
+      let { data: repos } = await octokit.rest.repos.listForUser({
         username: GITHUB_USERNAME,
       });
-      for (const repo of newRepos) {
+
+      repos = repos.filter((repo) => repo.name !== TEMPLATE_REPO_NAME);
+
+      // Set displayed name for each repo
+      for (const repo of repos) {
         repo.displayedName = startCase(decryptName(repo.name));
       }
-      newRepos.sort((r1, r2) =>
-        naturalCompare(r1.displayedName, r2.displayedName)
+
+      // Group repos
+      const map = {};
+      for (const repo of repos) {
+        const groupName = getGroupName(repo.displayedName);
+        if (map[groupName] == null) {
+          map[groupName] = [];
+        }
+        map[groupName].push(repo);
+      }
+
+      // Sort repos within group
+      for (const grouName of Object.keys(map)) {
+        const groupRepos = map[grouName];
+        groupRepos.sort((r1, r2) =>
+          naturalCompare(r1.displayedName, r2.displayedName)
+        );
+      }
+
+      const newRepoGroups = Object.entries(map);
+      newRepoGroups.sort(([groupName1], [groupName2]) =>
+        naturalCompare(groupName1, groupName2)
       );
-      setRepos(newRepos.filter((r) => r.name !== TEMPLATE_REPO_NAME));
+
+      setRepoGroups(newRepoGroups);
     })();
   }, []);
 
+  return repoGroups;
+}
+
+function App() {
+  const repoGroups = useRepoGroups();
+
   return (
     <div className="App">
+      {repoGroups.map(([groupName, repos]) => (
+        <AppGroup key={groupName} repos={repos} />
+      ))}
+    </div>
+  );
+}
+
+function AppGroup(props) {
+  const { repos } = props;
+
+  return (
+    <div className="App-group">
       {repos.map((repo) => (
         <AppLink key={repo.name} repo={repo} />
       ))}
